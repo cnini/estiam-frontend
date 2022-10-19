23a5681a434b570aa5283c9538f0635477e65cc3
Cynthia <cynthiazn.rabe@gmail.com>
2022-10-19 16:40:20 +0200

    Afficher une liste de tâches avec des données entrées en dur

--- tp/tp-01/src/routes/Tasks.js
+++ tp/tp-01/src/routes/Tasks.js
@@ -1,5 +1,20 @@
 export default function Tasks() {
+    const tasks = [
+        { id: 0, label: "Faire les courses" },
+        { id: 1, label: "Aller à la gym" },
+        { id: 2, label: "Dîner avec les parents" },
+        { id: 3, label: "RDV médical" },
+        { id: 4, label: "Faire le ménage" },
+        { id: 5, label: "Laver et étendre le linge" },
+        { id: 6, label: "Cours de piano" }
+    ]
+
     return (
-      <h1>Liste des tâches</h1>
+      <>
+        <h1>Liste des tâches</h1>
+        <ul>
+            { tasks.length ? tasks.map(task => (<li key={task.id}>{task.label}</li>)) : '' }
+        </ul>
+      </>
     )
 }
