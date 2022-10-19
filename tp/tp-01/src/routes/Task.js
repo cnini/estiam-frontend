d258cfa42f7727f002fb0c1bb76e2328dd62fe57
Cynthia <cynthiazn.rabe@gmail.com>
2022-10-19 17:06:51 +0200

    Afficher le détail d'une tâche en récupérant l'id dans l'URL grâce à useParams (react-router-dom)

--- tp/tp-01/src/routes/Task.js
+++ tp/tp-01/src/routes/Task.js
@@ -1,5 +1,24 @@
+import { useParams } from 'react-router-dom';
+
 export default function Task() {
+    const { id } = useParams()
+    
+    const tasks = [
+        { id: 0, label: "Faire les courses" },
+        { id: 1, label: "Aller à la gym" },
+        { id: 2, label: "Dîner avec les parents" },
+        { id: 3, label: "RDV médical" },
+        { id: 4, label: "Faire le ménage" },
+        { id: 5, label: "Laver et étendre le linge" },
+        { id: 6, label: "Cours de piano" }
+    ]
+
+    const task = tasks.find(task => task.id === +id)
+    
     return (
-      <h1>Page de détail d'une tâche</h1>
+      <ul>
+          <li><strong>Id :</strong> {task.id}</li>
+          <li><strong>Libellé :</strong> {task.label}</li>
+      </ul>
     )
 }
